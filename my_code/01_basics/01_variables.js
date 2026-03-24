6f219d7cb43367fb81035b097fe280babf77c97c
sejal <sakharwsejal@gmail.com>
2026-03-24 12:18:14 +0530

    second day

---
+++ my_code/01_basics/01_variables.js
@@ -0,0 +1,17 @@
+
+const accountId = 144556
+let accountEmail='htj@gamit.com'
+var accountPassword="1234"
+accountCity="Jaipur"
+
+let accountState;
+
+
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
+
+
+
---
+++ my_code/01_basics/02_dataTypes.js
@@ -0,0 +1,45 @@
+"use strict"; // tret all Js code as newer version
+
+// alert(3 + 3)
+
+
+
+
+
+let name = "hitesh"
+let age=28
+let isLoggedIn=false
+let state;
+
+//number => 2 to 53 
+//bigInt 
+//string =""
+
+// boolean  => true/false
+
+// null => standalone values
+// undefined
+// symbol => unique
+
+
+// object 
+
+console.log(typeof 'hi');
+
+log 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
