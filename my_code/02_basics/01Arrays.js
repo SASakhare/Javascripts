5c8f78255f6a7ceef5baf06d06cee90fdd2fb6f8
sejal <sakharwsejal@gmail.com>
2026-03-24 18:50:08 +0530

    Arrays

---
+++ my_code/02_basics/01Arrays.js
@@ -0,0 +1,80 @@
+
+// // array
+
+// const myArrat=[1,2,3,4,4,5,6,'sejal']
+// console.log(myArrat);
+
+// console.log(myArrat[0]);
+// console.log(myArrat[2]);
+
+
+// const myArra2=new Array(1,2,3,4,5)
+
+// console.log(myArra2);
+// console.log(myArra2.length);
+// console.log(myArra2.at(2));
+// console.log(myArra2.push(6));
+// console.log(myArra2);
+
+// console.log(myArra2.pop());
+// console.log(myArra2);
+
+
+
+// myArra2.unshift(9)
+// console.log(myArra2);
+// myArra2.shift()
+// console.log(myArra2);
+
+
+// console.log(myArra2.includes(9));
+// console.log(myArra2.indexOf(3));
+// console.log(myArra2.lastIndexOf(1));
+
+// const newArray=myArra2.join()
+// console.log(newArray)
+// console.log(typeof newArray)
+
+
+// * slice , splice
+
+const myArray=new Array(1,2,3,4,5)
+console.log("A ",myArray);
+
+const myn1=myArray.slice(1,3)
+
+console.log(myn1);
+console.log("B ",myArray);
+
+const myn2=myArray.splice(1,3)
+
+console.log(myn2);
+console.log("C ",myArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
