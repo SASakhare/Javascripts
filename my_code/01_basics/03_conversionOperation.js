4dbcce32c3f419807ac000dce9428d23a6f8b93f
sejal <sakharwsejal@gmail.com>
2026-03-24 12:27:29 +0530

    Data Conversion Operation

---
+++ my_code/01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+
+// let score = "33aa"
+let score = true
+
+console.log(typeof score);
+
+console.log(typeof(score));
+
+
+
+let valuesInNumber=Number(score)
+console.log(valuesInNumber);
+console.log(typeof(valuesInNumber));
+
+// let isLoggedIn=1
+let isLoggedIn=0
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+let someNumber=33
+
+let someString=String(someNumber)
+
+console.log(someString);
+console.log(typeof(someString));
+
