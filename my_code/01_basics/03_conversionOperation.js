365743b575e4a097f2dfab340b06d6ccec5aca17
sejal <sakharwsejal@gmail.com>
2026-03-24 12:37:01 +0530

    OperationBasics

--- my_code/01_basics/03_conversionOperation.js
+++ my_code/01_basics/03_conversionOperation.js
@@ -1,28 +1,69 @@
 
-// let score = "33aa"
-let score = true
+// // let score = "33aa"
+// let score = true
 
-console.log(typeof score);
+// console.log(typeof score);
 
-console.log(typeof(score));
-
-
-
-let valuesInNumber=Number(score)
-console.log(valuesInNumber);
-console.log(typeof(valuesInNumber));
-
-// let isLoggedIn=1
-let isLoggedIn=0
-
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(typeof(score));
 
 
-let someNumber=33
 
-let someString=String(someNumber)
+// let valuesInNumber=Number(score)
+// console.log(valuesInNumber);
+// console.log(typeof(valuesInNumber));
 
-console.log(someString);
-console.log(typeof(someString));
+// // let isLoggedIn=1
+// let isLoggedIn=0
+
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+// let someNumber=33
+
+// let someString=String(someNumber)
+
+// console.log(someString);
+// console.log(typeof(someString));
+
+
+
+// ******************************************* Operations *********************************************
+
+
+let value=3
+let negValues=-value
+
+console.log(value);
+console.log(negValues);
+
+
+console.log(2**3);
+console.log(3%4);
+
+let str1= "hello"
+let str2="hitesh"
+
+console.log(str1+str2);
+
+console.log("1"+3);
+console.log(1 + "3");
+console.log("1"+"3");
+
+console.log("1"+2+3);  // * left to right
+console.log(1+2+"3");
+
+
+console.log(true);
+
+
+console.log(+true);
+
+console.log(+"");
+
+
+
+
+
+
 
